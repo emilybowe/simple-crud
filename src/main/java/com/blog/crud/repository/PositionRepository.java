c67bc475cae456b54351d8e5ff149973330e9df0
Emily Bowe <ebowe@tcd.ie>
2020-11-04 12:30:54 +0000

    Annotate with @Repository for auto-detection and to catch persistence specific exceptions. Change class to interface.Extend the JPA interface to take advantage of the ready made CRUD methods.

--- src/main/java/com/blog/crud/repository/PositionRepository.java
+++ src/main/java/com/blog/crud/repository/PositionRepository.java
@@ -1,5 +1,10 @@
 package com.blog.crud.repository;
 
-public class PositionRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.blog.crud.domain.Position;
+
+@Repository
+public interface PositionRepository extends JpaRepository<Position, Long> {
 
 }
